43ca6fccaa78580ae135b6960026ba96f45eb2c7
Jeanne Suñga <jeannesunga02@gmail.com>
2023-02-02 16:20:49 +0800

    changed text to uppercase

--- src/units/MastHeader/MastHeader.js
+++ src/units/MastHeader/MastHeader.js
@@ -9,8 +9,8 @@ function MastHeader () {
         <img src= {CHEDlogo} alt='' height={150}/>
         </div>
         <p className='header-p'> 
-        <h1 className='republic'> Republic of the Philippines</h1>
-        <h2 className='president'> Office of the President     </h2>
+        <h1 className='republic'> REPUBLIC OF THE PHILIPPINES</h1>
+        <h2 className='president'> OFFICE OF THE PRESIDENT</h2>
         <h2 className='ched'> COMMISION ON HIGHER EDUCATION </h2>
         <h4 className='office'> REGIONAL OFFICE IV (CALABARZON)</h4>
         </p>
