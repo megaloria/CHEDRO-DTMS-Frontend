fc7271ee3a010397f65ec432196e7b0bde37150b
Darwin Lacson <lcsn.darwin@gmail.com>
2023-02-03 16:04:21 +0800

    creating login form day2

--- src/views/Login/Login.js
+++ src/views/Login/Login.js
@@ -1,50 +1,45 @@
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
-
+import './loginform.css';
+//import Image from 'react-bootstrap/Image'
 
 
 
 function loginform() {
   return (
+    <div>
+
+      <div className='con1'>
+      <Container fluid="md" style={{ height: '50vh', width: '29rem'}}>
+      <Card className='loginform' body style={{ marginTop: 'auto'}}>
+          <Form.Group className="mb-2" controlId="formGridEmail">
+          <Form.Label>Username</Form.Label>
+          <Form.Control placeholder="Username" />
+        </Form.Group>
+        <Form.Group className="mb-2" controlId="formGridPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control placeholder="Password" />
+        </Form.Group>
+            <Form.Group className="mb-3" id="formGridCheckbox">
+            <Form.Check type="checkbox" label="Remember password" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" id="formGridCheckbox">
+              <div className="d-grid gap-2">
+              <Button variant="primary" size="lg">Login Now</Button>
+              </div>
+        </Form.Group>
+        </Card>
+      </Container>
+      </div>
+    </div>
+
     
-    <Container fluid="md" style={{ height: '100vh' }}>
-
-        <Card body style={{ marginTop: 'auto'}}>
-      <Row>
-
-        <Col>
-        <Form.Group className="mb-2" controlId="formGridEmail">
-        <Form.Label>Email</Form.Label>
-        <Form.Control placeholder="Username" />
-      </Form.Group>
-
-      <Form.Group className="mb-2" controlId="formGridPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control placeholder="Password" />
-      </Form.Group>
-      <Form.Group className="mb-3" id="formGridCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
-
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-      </Col>
-
-
-        <Col></Col>
-      </Row>
-
-      </Card>
-
-
-    </Container>
-
+    
   );
+  
 }
 
 export default loginform;
