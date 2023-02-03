a7fe964d50361ae0add9a9ecabe28ffaa91d4fea
Vincebrian <vncbrian@gmail.com>
2023-02-03 16:50:15 +0800

    creating sidebar season 2

--- src/units/Sidebar/Sidebar.js
+++ src/units/Sidebar/Sidebar.js
@@ -1,31 +1,25 @@
 import React  from 'react';
 import "./Sidebar.css";
-import  SidebarData from './SidebarData'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { 
-    
-} from 'react-bootstrap'
+import { faGear, faNoteSticky, faTimes, faUserPlus } from '@fortawesome/free-solid-svg-icons'
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Nav from 'react-bootstrap/Nav';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-function Sidebar() {
-    return( 
-     <div className="Sidebar">
-        <ul className="SidebarList">
-            {SidebarData.map((val, key) => {
-            return ( 
-            <li key={key}
-            className="row"
-            onClick={()=>{
-                window.location.pathname = val.link;
-                }}
-                > 
-                <div id="icon">
-                &nbsp;<FontAwesomeIcon icon={val.icon}/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{val.title}
-                </div>
-            </li>
-            );
-            })}
-        </ul>
-    </div>
-    );
-}
-export default Sidebar;
+    function Sidebar() {
+        return(
+            
+            <div className="Sidebar">
+                <Nav defaultActiveKey="/home" className="flex">
+                    <Nav.Link a href="/document" className="flex1"><span style={{color:'white',fontSize:'15px'}}><FontAwesomeIcon icon = {faNoteSticky} />&nbsp;&nbsp;Documents</span></Nav.Link>
+                        <NavDropdown title= {<span style={{color:'white',fontSize:'15px'}}><FontAwesomeIcon icon ={faGear} />&nbsp;&nbsp;Settings</span>}  id="nav-dropdown" className="flex1">
+                            <NavDropdown.Item eventKey="4.1">Document Type</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item eventKey="4.2">HEIs</NavDropdown.Item>
+                        </NavDropdown>
+                    <Nav.Link href="/users" className="flex1">{<span style={{color:'white',fontSize:'15px'}}><FontAwesomeIcon icon = {faUserPlus}/>&nbsp;&nbsp;Users</span>}</Nav.Link>
+                </Nav>
+            </div>
+        );
+    }
+
+export default Sidebar
