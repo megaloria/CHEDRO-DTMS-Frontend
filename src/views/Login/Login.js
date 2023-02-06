1b4f56c616bad614fec91538bf12260319541dac
Darwin Lacson <lcsn.darwin@gmail.com>
2023-02-06 16:44:35 +0800

    login page UI done

--- src/views/Login/Login.js
+++ src/views/Login/Login.js
@@ -1,45 +1,55 @@
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
-import './loginform.css';
-//import Image from 'react-bootstrap/Image'
+import pic from '../../assets/CHEDL.png'
+
+import './loginform.css'
 
 
 
 function loginform() {
-  return (
-    <div>
-
-      <div className='con1'>
-      <Container fluid="md" style={{ height: '50vh', width: '29rem'}}>
-      <Card className='loginform' body style={{ marginTop: 'auto'}}>
-          <Form.Group className="mb-2" controlId="formGridEmail">
-          <Form.Label>Username</Form.Label>
-          <Form.Control placeholder="Username" />
-        </Form.Group>
-        <Form.Group className="mb-2" controlId="formGridPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control placeholder="Password" />
-        </Form.Group>
-            <Form.Group className="mb-3" id="formGridCheckbox">
-            <Form.Check type="checkbox" label="Remember password" />
-            </Form.Group>
-
-            <Form.Group className="mb-3" id="formGridCheckbox">
-              <div className="d-grid gap-2">
-              <Button variant="primary" size="lg">Login Now</Button>
-              </div>
-        </Form.Group>
-        </Card>
-      </Container>
-      </div>
-    </div>
-
-    
-    
-  );
+  return (    
   
+    <div style={{ height: '100vh', backgroundColor:'#0C2245'  }}>
+
+    <Container style={{ height: '100%'}}>
+      <Row style={{ height: '100%', alignItems: 'center'}}>
+        <Col style={{margin:'auto'}}md={{ span: 4 }}>
+        <Card className='p-3' style={{backgroundColor:'white',}}>
+        <Card.Body>
+      <div style={{ textAlign: 'center' }}>
+        <img src={pic} width='190' />
+      </div>
+
+      <Form.Group  className="Email" controlId="formGridEmail">
+            <Form.Label>Email</Form.Label>
+            <Form.Control  placeholder="Username" required />
+     </Form.Group>
+
+      <Form.Group className="Pass" controlId="formGridPassword">
+      <Form.Label>Password</Form.Label>
+            <Form.Control pill type ='password' placeholder="Password" required />
+      </Form.Group>
+      <Form.Group className= "cb" id="formGridCheckbox">
+            <Form.Check pill type="checkbox" label="Remember password" />
+      </Form.Group>
+      <br></br>
+      <div  className="d-grid gap-2">
+             <Button  variant="dark">Login Now</Button>
+      </div>
+
+      </Card.Body>
+      </Card>
+        </Col>
+        
+
+      </Row>
+    </Container>
+</div>
+  );
 }
 
 export default loginform;
