22d53930b8158209e08379dcaf05404f190653f3
Jeanne Suñga <jeannesunga02@gmail.com>
2023-02-07 08:46:05 +0800

    align blocks of code

--- src/views/Login/Login.js
+++ src/views/Login/Login.js
@@ -12,41 +12,48 @@ import './loginform.css'
 function loginform() {
   return (    
   
-    <div style={{ height: '100vh', backgroundColor:'#0C2245'  }}>
+  <div className='maindiv'>
 
-    <Container style={{ height: '100%'}}>
-      <Row style={{ height: '100%', alignItems: 'center'}}>
-        <Col style={{margin:'auto'}}md={{ span: 4 }}>
-          <Form>
-        <Card className='p-3' style={{backgroundColor:'white',}}>
-        <Card.Body>
-      <div style={{ textAlign: 'center', marginBottom:'1rem' }}>
-        <img src={pic} width='150' />
-      </div>
-      <Form.Group className="mb-3" controlId="formGridEmail">
-            <Form.Label>Username</Form.Label>
-            <Form.Control  placeholder="Username" required />
-     </Form.Group>
+    <Container className='Container1'>
 
-      <Form.Group className="mb-3" controlId="formGridPassword">
-      <Form.Label>Password</Form.Label>
-            <Form.Control pill type ='password' placeholder="Password" required />
-      </Form.Group>
-      <Form.Group className= "mb-3" id="formGridCheckbox">
-            <Form.Check pill type="checkbox" label="Remember password" />
-      </Form.Group>
-      <div  className="d-grid gap-2">
-             <Button  variant="primary">Login</Button>
-      </div>
+    <Row style={{ height: '100%', alignItems: 'center'}}>
 
-      </Card.Body>
-      </Card>
+    <Col style={{margin:'auto'}}md={{ span: 4 }}>
+
+      <Form>
+
+       <Card className='p-3' style={{backgroundColor:'white',}}>
+          
+          <Card.Body>
+
+            <div className='logo'>
+              <img src={pic} width='150' />
+            </div>
+
+              <Form.Group className="mb-3" controlId="formGridEmail">
+                    <Form.Label>Username</Form.Label>
+                    <Form.Control  placeholder="Username" required />
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="formGridPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control pill type ='password' placeholder="Password" required />
+              </Form.Group>
+
+              <Form.Group className= "mb-3" id="formGridCheckbox">
+                    <Form.Check pill type="checkbox" label="Remember password" />
+              </Form.Group>
+
+              <div  className="d-grid gap-2">
+                    <Button  variant="primary">Login</Button>
+              </div>
+
+            </Card.Body>
+        </Card>
       </Form>
-        </Col>
-        
-
-      </Row>
-    </Container>
+    </Col>
+    </Row>
+  </Container>
 </div>
   );
 }
