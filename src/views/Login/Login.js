0a8ba59eee63185c96b76d250aeabae5ba6104bf
Darwin Lacson <lcsn.darwin@gmail.com>
2023-02-08 15:56:15 +0800

    change l to L

--- src/views/Login/Login.js
+++ src/views/Login/Login.js
@@ -5,7 +5,7 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
 import pic from '../../assets/CHEDL.png'
-import './Loginform.css'
+import './loginform.css'
 import { useState } from 'react';
 import Validator from 'validatorjs';
 
