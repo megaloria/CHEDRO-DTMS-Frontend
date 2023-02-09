5f424604cb592e918c1d62aa8dfb1ea496e8383a
Darwin Lacson <lcsn.darwin@gmail.com>
2023-02-09 11:19:13 +0800

    CHANGE PASSWORD WITH VALIDATION

--- src/views/ChangePassword/ChangePassword.js
+++ src/views/ChangePassword/ChangePassword.js
@@ -11,15 +11,19 @@ import Validator from 'validatorjs';
 
 
 
-function Loginform() {
+function Changepassword() {
   const [values, setValues] = useState({
-    username: '',
-    password: ''
+          Currentpass:  '',
+          Newpass:      '',
+          Confirmpass:  ''
   });
     
     const [errors, setError] = useState({
-      username: '',
-      password: ''
+
+         Currentpass:  '',
+          Newpass:      '',
+          Confirmpass:  ''
+     
     });
 
       function handleChange(e) {
@@ -33,19 +37,23 @@ function Loginform() {
         e.preventDefault();
 
         let validation = new Validator(values, {
-          username: 'required|min:3',
-          password: 'required|min:8'
+          Currentpass:  'required',
+          Newpass:      'required|min:8',
+          Confirmpass:  'required|min:8'
         });
-
+       
         if (validation.fails()) {
           setError({
-            username: validation.errors.first('username'),
-            password: validation.errors.first('password'),
-          });
+            Currentpass: validation.errors.first('Currentpass'),
+            Newpass: validation.errors.first('Newpass'),
+            Confirmpass: validation.errors.first('Confirmpass'),
+              });
         } else {
           setError({
-            username: '',
-            password: ''
+            Currentpass: '',
+             Newpassword: '',
+             Confirmpass:''
+
           });
         }
       }
@@ -68,67 +76,67 @@ function Loginform() {
               <img src={pic} width='150' />
             </div> */}
 
+            
           <Form.Group 
               className='mb-3' 
-              value={values.username}
-              controlId='formGridusername'
-              onChange={handleChange}>
-
-          <Form.Label>Username</Form.Label>
-          <Form.Control 
-              placeholder='Username'
-              name='username' 
-              isInvalid = {!!errors.username} />
-              <Form.Control.Feedback type='invalid'>
-              {errors.username}
-          </Form.Control.Feedback>  
-          </Form.Group>
-
-
-          <Form.Group 
-              className='mb-3' 
-              value={values.password}
+              value={values.Curentpassword}
               controlId='formGridpassword'
               onChange={handleChange}>
 
-          <Form.Label>Password</Form.Label>
+          <Form.Label>Current password</Form.Label>
           <Form.Control 
           type='password'
-              placeholder='Password'
-              name='password' 
-              isInvalid = {!!errors.password} />
+              placeholder='Current password'
+              name='Currentpass' 
+              isInvalid = {!!errors.Currentpass} />
               <Form.Control.Feedback type='invalid'>
-              {errors.password}
+              {errors.Currentpass}
           </Form.Control.Feedback>  
           </Form.Group>
-          {/* <Form.Group 
-                className='mb-3'
-                value={values.password}
-                controlId='formGridpassword'
-                onChange={handleChange} >      
-              <Form.Label>Password</Form.Label>
-             
-              <Form.Control 
-                type='password'
-                placeholder='Password'
-                name='password'
-                isInvalid = {!!errors.password} />
-              <Form.Control.Feedback type='invalid'>
-                {errors.password}
-              </Form.Control.Feedback>
-           </Form.Group> */}
 
-              <Form.Group 
-                className= 'mb-3'
-                controlId='rememberPassword'>
-              <Form.Check 
-                pill type='checkbox' 
-                label='Remember password'/>
-              </Form.Group>
+         
+         
+<Form.Group 
+              className='mb-3' 
+              value={values.Newpass}
+              controlId='formGridpassword'
+              onChange={handleChange}>
+
+          <Form.Label>New password</Form.Label>
+          <Form.Control 
+          type='password'
+              placeholder='New Password'
+              name='Newpass' 
+              isInvalid = {!!errors.Newpass} />
+              <Form.Control.Feedback type='invalid'>
+              {errors.Newpass}
+          </Form.Control.Feedback>  
+          </Form.Group>
+
+          
+          <Form.Group 
+              className='mb-3' 
+              value={values.Confirmpass}
+              controlId='formGridpassword'
+              onChange={handleChange}>
+
+          <Form.Label>Confirm password</Form.Label>
+          <Form.Control 
+          type='password'
+              placeholder='Confirm password'
+              name='Confirm password' 
+              isInvalid = {!!errors.Confirmpass}/>
+              <Form.Control.Feedback type='invalid'>
+              {errors.Confirmpass}
+          </Form.Control.Feedback>  
+          </Form.Group>
+
+
+            
               <div  className='d-grid gap-2'>
               <Button
                 type='submit' 
-                variant='primary'>Login
+                variant='dark'> save
             </Button>
               </div>   
 
@@ -142,4 +150,4 @@ function Loginform() {
   );
 }
 
-export default Loginform;
+export default Changepassword;
