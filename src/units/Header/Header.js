f5aaa6125fdc06b4d3675e4f305cd28227404249
Daryl Eine Manalo <demanalo@ched.gov.ph>
2023-02-14 16:16:32 +0800

    Organized editor styles

--- src/units/Header/Header.js
+++ src/units/Header/Header.js
@@ -1,25 +1,26 @@
-// import React, { useState } from "react";
-import "./Header.css";
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from 'react';
+import {
+  Container,
+  Nav,
+  Navbar,
+  NavDropdown,
+  OverlayTrigger,
+  Tooltip
+} from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faBell,
-  faCircleExclamation,
   faDoorOpen,
   faKey,
   faUser
-} from "@fortawesome/free-solid-svg-icons";
-import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
-import Tooltip from 'react-bootstrap/Tooltip';
+} from '@fortawesome/free-solid-svg-icons';
+
+import './styles.css';
 
 function Header() {
 
-
-  const renderTooltip = (props) => (
-    <Tooltip id="button-tooltip" {...props}>
+  const renderTooltip = props => (
+    <Tooltip id='button-tooltip' {...props}>
       Home
     </Tooltip>
   );
@@ -27,67 +28,54 @@ function Header() {
   return (
     <Navbar
       collapseOnSelect
-      expand="lg"
-      bg="white"
-      variant="light"
-      sticky="top">
-      <Container className="cont1">
+      expand='lg'
+      variant='light'
+      sticky='top'>
+      <Container className='cont1'>
         <OverlayTrigger
-          placement="right"
+          placement='right'
           delay={{ show: 250, hide: 200 }}
-          overlay={renderTooltip}
-        >
-          <Navbar.Brand className="title" href="#home">
-            CHED Document Tracking Management System
+          overlay={renderTooltip}>
+          <Navbar.Brand className='title' href='#home'>
+            <span className='d-none d-md-inline-block'>
+              CHED Document Tracking Management System
+            </span>
+            <span className='d-inline-block d-md-none'>
+              CHED DTMS
+            </span>
           </Navbar.Brand>
         </OverlayTrigger>
 
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="ms-auto">
+        <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+        <Navbar.Collapse id='responsive-navbar-nav'>
+          <Nav className='ms-auto'>
             <NavDropdown
               title={
                 <span>
-                  <FontAwesomeIcon
-                    icon={faBell}
-                  /> Notifications
+                  <FontAwesomeIcon icon={faBell} /> Notifications
                 </span>
               }
-              id="collasible-nav-dropdown"
-              renderMenuOnMount={true}
-            >
-              <NavDropdown.Item className="ms-auto" href=" "
-              >
-                <FontAwesomeIcon
-                  icon={faCircleExclamation}
-                  fixedWidth
-                /> Notifications here
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
+              id='collasible-nav-dropdown'
+              renderMenuOnMount={true}>
             </NavDropdown>
             <NavDropdown
               title={
                 <span>
-                  <FontAwesomeIcon
-                    icon={faUser}
-                  /> Username
+                  <FontAwesomeIcon icon={faUser} /> Username
                 </span>
               }
-              id="collasible-nav-dropdown"
-              renderMenuOnMount={true}
-            >
-              <NavDropdown.Item href=" ">
+              id='collasible-nav-dropdown'
+              renderMenuOnMount={true}>
+              <NavDropdown.Item href=' '>
                 <FontAwesomeIcon
                   icon={faKey}
-                  fixedWidth
-                /> Change Password
+                  fixedWidth /> Change Password
               </NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href=" ">
+              <NavDropdown.Item href=' '>
                 <FontAwesomeIcon
                   icon={faDoorOpen}
-                  fixedWidth
-                /> Logout
+                  fixedWidth /> Logout
               </NavDropdown.Item>
             </NavDropdown>
           </Nav>
@@ -96,4 +84,5 @@ function Header() {
     </Navbar>
   );
 }
+
 export default Header;
