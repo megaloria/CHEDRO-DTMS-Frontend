903eab66803b663fabe14404018646366c08d1cf
Daryl Eine Manalo <demanalo@ched.gov.ph>
2023-02-14 15:04:40 +0800

    Removed unnecessary editor styling

--- src/views/Login/Login.js
+++ src/views/Login/Login.js
@@ -1,145 +1,120 @@
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Card from 'react-bootstrap/Card';
-import pic from '../../assets/CHEDL.png'
-import './loginform.css'
 import { useState } from 'react';
+import {
+  Button,
+  Card,
+  Col,
+  Container,
+  Form,
+  Row
+} from 'react-bootstrap';
 import Validator from 'validatorjs';
 
+import pic from '../../assets/CHEDL.png'
+import './styles.css'
 
-
-function Loginform() {
+function Login() {
   const [values, setValues] = useState({
     username: '',
     password: ''
   });
     
-    const [errors, setError] = useState({
-      username: '',
-      password: ''
+  const [errors, setError] = useState({
+    username: '',
+    password: ''
+  });
+
+  function handleChange(e) {
+    setValues({
+      ...values,
+      [e.target.name]: e.target.value
+    });
+  }
+
+  function handleSubmit(e){
+    e.preventDefault();
+
+    let validation = new Validator(values, {
+      username: 'required|min:3',
+      password: 'required|min:8'
     });
 
-      function handleChange(e) {
-        setValues({
-          ...values,
-          [e.target.name]: e.target.value
-        });
-      }
-
-      function handleSubmit(e){
-        e.preventDefault();
-
-        let validation = new Validator(values, {
-          username: 'required|min:3',
-          password: 'required|min:8'
-        });
-
-        if (validation.fails()) {
-          setError({
-            username: validation.errors.first('username'),
-            password: validation.errors.first('password'),
-          });
-        } else {
-          setError({
-            username: '',
-            password: ''
-          });
-        }
-      }
+    if (validation.fails()) {
+      setError({
+        username: validation.errors.first('username'),
+        password: validation.errors.first('password'),
+      });
+    } else {
+      setError({
+        username: '',
+        password: ''
+      });
+    }
+  }
 
   return (    
-  
-  <div className='maindiv'>
+    <div className='maindiv'>
+      <Container className='container1'>
+        <Row style={{ height: '100%', alignItems: 'center'}}>
+          <Col style={{margin:'auto'}}md={{ span: 4 }}>
+            <Form onSubmit={handleSubmit}>
+              <Card className='p-3' style={{borderRadius:'25px'}}>
+                <Card.Body>
+                  <div className='logo'>
+                    <img src={pic} width='150' />
+                  </div>
 
-    <Container className='container1'>
+                  <Form.Group 
+                      className='mb-3' 
+                      value={values.username}
+                      controlId='formGridusername'
+                      onChange={handleChange}>
+                    <Form.Label>Username</Form.Label>
+                    <Form.Control 
+                        placeholder='Username'
+                        name='username' 
+                        isInvalid = {!!errors.username} />
+                        <Form.Control.Feedback type='invalid'>
+                        {errors.username}
+                    </Form.Control.Feedback>  
+                  </Form.Group>
+                  <Form.Group 
+                      className='mb-3' 
+                      value={values.password}
+                      controlId='formGridpassword'
+                      onChange={handleChange}>
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control 
+                    type='password'
+                        placeholder='Password'
+                        name='password' 
+                        isInvalid = {!!errors.password} />
+                        <Form.Control.Feedback type='invalid'>
+                        {errors.password}
+                    </Form.Control.Feedback>  
+                  </Form.Group>
+                  <Form.Group 
+                    className= 'mb-3'
+                    controlId='rememberPassword'>
+                    <Form.Check 
+                      pill type='checkbox' 
+                      label='Remember password'/>
+                  </Form.Group>
 
-    <Row style={{ height: '100%', alignItems: 'center'}}>
-
-    <Col style={{margin:'auto'}}md={{ span: 4 }}>
-
-      <Form onSubmit={handleSubmit}>
-
-       <Card className='p-3' style={{borderRadius:'25px'}}>
-          <Card.Body>
-            <div className='logo'>
-              <img src={pic} width='150' />
-            </div>
-
-          <Form.Group 
-              className='mb-3' 
-              value={values.username}
-              controlId='formGridusername'
-              onChange={handleChange}>
-
-          <Form.Label>Username</Form.Label>
-          <Form.Control 
-              placeholder='Username'
-              name='username' 
-              isInvalid = {!!errors.username} />
-              <Form.Control.Feedback type='invalid'>
-              {errors.username}
-          </Form.Control.Feedback>  
-          </Form.Group>
-
-
-          <Form.Group 
-              className='mb-3' 
-              value={values.password}
-              controlId='formGridpassword'
-              onChange={handleChange}>
-
-          <Form.Label>Password</Form.Label>
-          <Form.Control 
-          type='password'
-              placeholder='Password'
-              name='password' 
-              isInvalid = {!!errors.password} />
-              <Form.Control.Feedback type='invalid'>
-              {errors.password}
-          </Form.Control.Feedback>  
-          </Form.Group>
-          {/* <Form.Group 
-                className='mb-3'
-                value={values.password}
-                controlId='formGridpassword'
-                onChange={handleChange} >      
-              <Form.Label>Password</Form.Label>
-             
-              <Form.Control 
-                type='password'
-                placeholder='Password'
-                name='password'
-                isInvalid = {!!errors.password} />
-              <Form.Control.Feedback type='invalid'>
-                {errors.password}
-              </Form.Control.Feedback>
-           </Form.Group> */}
-
-              <Form.Group 
-                className= 'mb-3'
-                controlId='rememberPassword'>
-              <Form.Check 
-                pill type='checkbox' 
-                label='Remember password'/>
-              </Form.Group>
-              <div  className='d-grid gap-2'>
-              <Button
-                type='submit' 
-                variant='primary'>Login
-            </Button>
-              </div>   
-
-            </Card.Body>
-        </Card>
-      </Form>
-    </Col>
-    </Row>
-  </Container>
-</div>
+                  <div  className='d-grid gap-2'>
+                    <Button
+                      type='submit' 
+                      variant='primary'>
+                      Login
+                    </Button>
+                  </div>
+                </Card.Body>
+              </Card>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
+    </div>
   );
 }
 
-export default Loginform;
+export default Login;
