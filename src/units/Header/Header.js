ec0ca84cc57bac52dc5c6dbc4fe41c6af1dbbb79
Roel Cristobal <rdcristobal016@gmail.com>
2023-02-14 09:25:10 +0800

    modal-logout

--- src/units/Header/Header.js
+++ src/units/Header/Header.js
@@ -1,22 +1,31 @@
 // import React, { useState } from "react";
 import "./Header.css";
+import React, { useState } from 'react';
 import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { 
+    Nav,
+    Navbar,
+    NavDropdown,
+    OverlayTrigger,
+    Tooltip,
+    Modal,
+    Button
+    } from "react-bootstrap";
 import {
-  faBell,
-  faCircleExclamation,
-  faDoorOpen,
-  faKey,
-  faUser
-} from "@fortawesome/free-solid-svg-icons";
-import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
-import Tooltip from 'react-bootstrap/Tooltip';
+    faBell,
+    faCircleExclamation,
+    faDoorOpen,
+    faKey,
+    faUser
+    } from "@fortawesome/free-solid-svg-icons";
 
 function Header() {
 
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 
   const renderTooltip = (props) => (
     <Tooltip id="button-tooltip" {...props}>
@@ -76,24 +85,44 @@ function Header() {
               id="collasible-nav-dropdown"
               renderMenuOnMount={true}
             >
-              <NavDropdown.Item href=" ">
+              <NavDropdown.Item href="#">
                 <FontAwesomeIcon
                   icon={faKey}
                   fixedWidth
                 /> Change Password
               </NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href=" ">
+              <NavDropdown.Item href="#">
+                <Button variant="outline" onClick={handleShow}>
                 <FontAwesomeIcon
                   icon={faDoorOpen}
                   fixedWidth
                 /> Logout
+                </Button>
+
+                <Modal show={show} onHide={handleClose} animation={false}>
+                    <Modal.Header closeButton>
+                    <Modal.Title>Logout</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>Are you sure you want to logout?</Modal.Body>
+                    <Modal.Footer>
+                    <Button variant="outline-danger" onClick={handleClose}>
+                        Cancel
+                    </Button>
+                    <Button variant="primary" onClick={handleClose}>
+                        Logout
+                    </Button>
+                    </Modal.Footer>
+                </Modal>
+
               </NavDropdown.Item>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
+
+
   );
 }
 export default Header;
