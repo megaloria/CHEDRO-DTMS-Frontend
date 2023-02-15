f6abe1b17e51423430f605a91b932c18205dfe8f
Jeanne Suñga <jeannesunga02@gmail.com>
2023-02-15 09:56:20 +0800

    added styles

--- src/views/ErrorPage/ErrorPage.js
+++ src/views/ErrorPage/ErrorPage.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import img from '../../assets/404.png';
 
 function ErrorPage() {
   return (
-    	<div style={{textAlign:'center'}}>
-		<h1>404 Error</h1>
-		<h1> Sorry Page Not Found</h1>
-	</div>
+    	<>
+		<div style={{marginTop: '10%', textAlign: 'center' }}>
+
+			<div>
+			<img style={{width: '500px', height: 'auto' }} src={img} alt='' />
+			</div>
+
+	  		<div style={{marginTop:'2%'}}>
+			  <h1 style={{ fontSize: '90px' }}> <b> Oops!</b> </h1>
+			  <h3> Sorry Page Not Found </h3>
+			  <h5> The link you clicked may be broken or the page may have been removed. </h5>
+		  	</div> 
+
+		</div>
+		</>
   );
 }
 
 export default ErrorPage;
+
