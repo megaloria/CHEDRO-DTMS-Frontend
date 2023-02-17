504f948e66d49cc95fe107402d75e72ffee9e0fc
VinceBrianLimbo <binslimbo@gmail.com>
2023-02-17 09:53:36 +0800

    adding icon in footer

--- src/components/units/Footer/Footer.js
+++ src/components/units/Footer/Footer.js
@@ -1,16 +1,28 @@
 import React from 'react';
+import { FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+
+import {
+    faFacebookSquare
+  } from '@fortawesome/free-brands-svg-icons';
 
 import './styles.css';
 
 function Footer(){
     return(
         <div className='Footer'>
-        <div className='footer'>
-            City Hall Compound, Barangay Marawoy, Jose P. Laurel Highway, Lipa City
-        </div>
-        <div className='footer'>
-            ched4a@ched.gov.ph / (043) 727-2764
-         </div>
+            <div className='footer'>
+                City Hall Compound, Barangay Marawoy, Jose P. Laurel Highway, Lipa City
+            </div>
+            
+            <div className='footer'>
+                ched4a@ched.gov.ph / (043) 727-2764
+            </div>
+
+            <div className='footer'>
+                <a href="https://www.facebook.com/ched4/">
+                    <FontAwesomeIcon icon={faFacebookSquare} />
+                </a>
+            </div>
         </div>
     );  
 }
