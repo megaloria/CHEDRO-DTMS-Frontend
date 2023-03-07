c3697092071dcfdd472a020d12a68748ed364734
Roel Cristobal <rdcristobal016@gmail.com>
2023-03-07 10:51:03 +0800

    edited radio button

--- src/components/views/Documents/admin/DocumentReceive.js
+++ src/components/views/Documents/admin/DocumentReceive.js
@@ -8,7 +8,7 @@ import {
 } from 'react-bootstrap';
 import {
     Link
-} from 'react-router-dom';
+} from 'react-router-dom'
 import moment from 'moment';
 import apiClient from '../../../../helpers/apiClient';
 
@@ -19,9 +19,6 @@ function DocumentReceive() {
     const [selectedOption, setSelectedOption] = useState('');
     const [selectedOption2, setSelectedOption2] = useState('');
     const [selectedOption3, setSelectedOption3] = useState('');
-    
-    const [selectedOption4, setSelectedOption4] = useState("");
-    const [selectForm, setSelectForm] = useState(null);
     const [provinces, setProvinces] = useState([]);
     const [municipalities, setMunicipalities] = useState([]);
     const [names, setNames] = useState([]);
@@ -62,15 +59,6 @@ function DocumentReceive() {
           const response = await apiClient.get('/settings/heis/names');
           setNames(response.data.data);
         } 
-      }
-
-      function handleOptionChange(event) {
-        setSelectedOption4(event.target.value);
-      }
-
-      function handleAddSelectForm() {
-        setSelectForm(
-        );
       }
 
     //Document Types
@@ -231,7 +219,6 @@ function DocumentReceive() {
                     <Form.Control as="textarea" rows={5} type="text" placeholder="Description" />
                 </Col>
             </Row>
-
             <Row className="mb-3"> 
             <Form.Group>
                 <div> 
@@ -248,32 +235,13 @@ function DocumentReceive() {
                                 id={`inline-${category}-1`}
                                 value={category.id}
                                 key={category.id}
-                                onChange={handleOptionChange}
                             />  
                             {category.description}
                             
                         </div>
                     ))}
                 </Form.Group>
-                    {selectedOption4 && (
-                        <div>
-                        {selectForm ? (
-                            <div>{selectForm}</div>
-                        ) : (
-                            <Row> 
-                                <Col md={3}> 
-                            <Form.Control onClick={handleAddSelectForm}
-                                type='text'
-                                placeholder=''
-                                readOnly
-                            />
-                            </Col>
-                            </Row>
-                        )}
-                        </div>
-                    )}    
             </Row>
-            
             <div>
                 <Row className= "justify-content-end mt-4 mb-3">
                     <Col md="auto" className="p-0">
@@ -285,7 +253,7 @@ function DocumentReceive() {
                         </Button>
                     </Col>
                     <Col md="auto">
-                        <Button variant="primary">
+                        <Button variant="outline-primary">
                             Received
                         </Button>
                     </Col>
