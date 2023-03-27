aeb5fc61a2001682aa9ea8cfcb9272c8efba65ac
Jeanne Suñga <jeannesunga02@gmail.com>
2023-03-27 13:25:29 +0800

    modified the search and add icon of CHED offices for responsiveness

--- src/components/views/CHED/CHED.js
+++ src/components/views/CHED/CHED.js
@@ -292,33 +292,38 @@ function CHED() {
 
     return (
         <Container fluid>
-            <div className='crud bg-body rounded'>
+            <div className='bg-body rounded'> 
                 <Row className= 'justify-content-end mt-4 mb-3'>
                     <Col>
                         <h1>CHED Offices</h1>
                     </Col>
-                    <Col md="auto">
-                        <div className="search">
+                </Row> 
+
+                <div> 
+                    <div className='d-md-flex mb-3 justify-content-end'>
+                        <div className='search'> 
                             <Form className="d-flex" controlId="" onSubmit={handleSearch}>
-                                <Form.Control 
-                                    type="search" 
-                                    placeholder="Search" 
-                                    className="me-2"
-                                    value={searchQuery}
-                                    onChange={handleSearchInputChange}
-                                />
-                                <Button type='submit'>
-                                    <FontAwesomeIcon icon={faSearch} />
-                                </Button>
+                                    <Form.Control 
+                                        type="search" 
+                                        placeholder="Search" 
+                                        className="me-2"
+                                        value={searchQuery}
+                                        onChange={handleSearchInputChange}
+                                    />
+                                    <Button type='submit'>
+                                        <FontAwesomeIcon icon={faSearch} />
+                                    </Button>
+
+                                    <div className='ms-2'> 
+                                        <Button variant='primary' onClick={e => handleShowModal()} style={{whiteSpace:'nowrap'}}>
+                                            <FontAwesomeIcon icon={faAdd}/> 
+                                            <span className='d-none d-md-inline-block ms-1'> Add </span>
+                                        </Button>
+                                    </div>
                             </Form>
                         </div>
-                    </Col>
-                    <Col md='auto'>
-                        <Button variant='primary' onClick={e => handleShowModal()}>
-                            <FontAwesomeIcon icon={faAdd} /> Add
-                        </Button>
-                    </Col>  
-                </Row>
+                    </div>
+                </div>    
             </div>
 
             {
