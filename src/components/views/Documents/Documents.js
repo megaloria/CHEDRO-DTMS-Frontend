5e0461fe498fca97a89ce253aec16f661ae055f2
Maria Dimaano <dimaanomariaisabelle@gmail.com>
2023-04-12 11:26:24 +0800

    removed the conditional rendering of forward button

--- src/components/views/Documents/Documents.js
+++ src/components/views/Documents/Documents.js
@@ -441,11 +441,9 @@ function Documents() {
                                                                 <FontAwesomeIcon icon={faCircleArrowRight} className="" /> View
                                                             </Button>
                                                         
-                                                            {row.category_id === 1 || row.category_id === 2 ? (
                                                                 <Button variant="link" size='sm' onClick={e => handleShowModal(row)}>
                                                                 <FontAwesomeIcon icon={faShare} className="" />
                                                             </Button>
-                                                            ) : null}
 
                                                             <Button variant="link" size='sm' as={Link} to={`edit/${row.id}`} >
                                                                 <FontAwesomeIcon icon={faEdit} className="text-success" />
@@ -592,11 +590,9 @@ function Documents() {
                                                             <FontAwesomeIcon icon={faCircleArrowRight} className="" /> View
                                                         </Button>
 
-                                                        {row.category_id === 1 || row.category_id === 2 ? (
                                                             <Button variant="link" size='sm' onClick={e => handleShowModal(row)}>
                                                                 <FontAwesomeIcon icon={faShare} className="" />
                                                             </Button>
-                                                        ) : null}
 
                                                         <Button variant="link" size='sm' as={Link} to={`edit/${row.id}`} >
                                                             <FontAwesomeIcon icon={faEdit} className="text-success" />
