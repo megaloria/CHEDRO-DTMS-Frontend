712466c54321c29c3a341fae4c81d6f0482d6c6a
Jeanne Suñga <jeannesunga02@gmail.com>
2023-05-12 14:41:43 +0800

    Adjusted the tag of timeline to make it responsive ---used icon on small screen instead of tag

--- src/components/units/Timeline/TimelineItem.js
+++ src/components/units/Timeline/TimelineItem.js
@@ -1,16 +1,29 @@
+import { faUserCheck } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React  from 'react';
+import {
+  Button
+} from 'react-bootstrap';
 
 const TimelineItem = ({ data }) => (
     <div className="timeline-item" {...data}>
-        <div className="timeline-item-content" style={{ }}>
-            <div>
-                <span className="tag" style={{ background: data.category?.color }}>
+        <div className="timeline-item-content" >
+              <div style={{textAlign:'right', width:'100%', marginTop:'-5px', marginRight:'-10px'}}>
+                <span className="tag d-none d-md-inline-block" >
                     {data.category?.tag}
-                </span>
-                <time>{data.date}</time>
-                <p>{data.text}</p>
-                
+                </span> 
+                <span className='d-inline-block d-md-none'>
+                <Button variant="outline-warning" size='sm'>
+                        <FontAwesomeIcon icon={faUserCheck} className=""/> </Button>
+                 </span>
+             </div> 
+            
+            <div>
+                <time>{data.date}</time>    
+                <p>{data.text}</p>   
             </div>
+         
+             
             <span className="circle" style={data.circleStyle}/>
                 {/* , backgroundColor: data.circleColor  */}
         </div>
