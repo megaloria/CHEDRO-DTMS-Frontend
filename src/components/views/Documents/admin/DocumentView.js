315e08e4cdc3e53f3e25dd96ecf8297fc7804188
Jeanne Suñga <jeannesunga02@gmail.com>
2023-05-16 14:42:11 +0800

    displayed comment of approved, rejected,acted in timeline --admin

--- src/components/views/Documents/admin/DocumentView.js
+++ src/components/views/Documents/admin/DocumentView.js
@@ -29,7 +29,8 @@ import {
     faTag,
     faBuildingUser,
     faClock,
-    faUserCheck
+    faUserCheck,
+    faQuoteRight
 } from '@fortawesome/free-solid-svg-icons'
 import {
     Link, useLoaderData, useNavigate, useLocation
@@ -150,19 +151,26 @@ function DocumentView() {
         </>
     );
 
-    const ActionedUsersText = ({ users }) => (
+    const ActionedUsersText = ({ users, log }) => (
         <>
-            Document actioned by:{" "}
+            Document acted by:{" "}
             {users.map((user, index) => (
                 <p key={user?.id}>
                     {user?.name} - <i>{user?.position_designation}</i>
                     {index !== users.length - 1 ? ", " : ""}
                 </p>
             ))}
+             {
+                log.comment && (
+                    <span className='comment-text'>
+                        <FontAwesomeIcon icon={faQuoteLeft} className='quote-left'/> {log.comment} <FontAwesomeIcon icon={faQuoteRight} className='quote-left'/>
+                    </span>
+                )
+            }
         </>
     );
 
-    const ApprovedUsersText = ({ users }) => (
+    const ApprovedUsersText = ({ users, log }) => (
         <>
             Document approved by:{" "}
             {users.map((user, index) => (
@@ -171,10 +179,17 @@ function DocumentView() {
                     {index !== users.length - 1 ? ", " : ""}
                 </p>
             ))}
+               {
+                log.comment && (
+                    <span className='comment-text'>
+                        <FontAwesomeIcon icon={faQuoteLeft} className='quote-left' /> {log.comment} <FontAwesomeIcon icon={faQuoteRight} className='quote-left'/>
+                    </span>
+                )
+            }
         </>
     );
 
-    const RejectedUsersText = ({ users }) => (
+    const RejectedUsersText = ({ users, log }) => (
         <>
             Document rejected by:{" "}
             {users.map((user, index) => (
@@ -183,6 +198,13 @@ function DocumentView() {
                     {index !== users.length - 1 ? ", " : ""}
                 </p>
             ))}
+               {
+                log.comment && (
+                    <span className='comment-text'>
+                        <FontAwesomeIcon icon={faQuoteLeft} className='quote-left' /> {log.comment} <FontAwesomeIcon icon={faQuoteRight} className='quote-left'/>
+                    </span>
+                )
+            }
         </>
     );
 
