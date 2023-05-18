93ab8139bca918b13778b11df4474d57913d7266
Jeanne Suñga <jeannesunga02@gmail.com>
2023-05-18 15:55:45 +0800

    modified ack and rejected badges color and texts

--- src/components/views/Documents/user/DocumentsUser.js
+++ src/components/views/Documents/user/DocumentsUser.js
@@ -321,7 +321,7 @@ function DocumentsUser() {
             text: 'You won\'t be able to revert this!',
             icon: 'warning',
             showCancelButton: true,
-            confirmButtonColor: '#3085d6',
+            confirmButtonColor: '#7066e0',
             cancelButtonColor: '#d33',
             confirmButtonText: 'Yes, acknowledge it!',
             reverseButtons: true,
