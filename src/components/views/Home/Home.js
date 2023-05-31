0640b22baf4b7a713d07a35875d1561e7a9b4435
Daryl Eine Manalo <demanalo@ched.gov.ph>
2023-05-31 17:41:31 +0800

    Temporary commit

--- src/components/views/Home/Home.js
+++ src/components/views/Home/Home.js
@@ -16,7 +16,7 @@ import Echo from 'laravel-echo';
 import Pusher from 'pusher-js';
 
 window.Pusher = Pusher;
- 
+
 window.Echo = new Echo({
   broadcaster: 'pusher',
   key: process.env['REACT_APP_PUSHER_KEY'],
